97b9256bdc84ee8ec62de78104a3b981f7f5d7c5
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-03-20 14:17:00 +0900

    음식 종류 버튼 생성

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const FooterApp = () => {
-  return <div> 이곳은 푸터 자라입니다</div>;
+  return (
+    <footer id="footer">
+      <div class="inner_footer">
+        <span> &copy; AmuGuna</span>
+      </div>
+    </footer>
+  );
 };
 
 export default FooterApp;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,39 @@
 import React from "react";
+import "../components/css/main.css";
+import { Link } from "react-router-dom";
 
 const MainApp = () => {
-  return <div>메인입니다</div>;
+  return (
+    <div className="main-content">
+      <div className="content-step1">
+        <h2 className="food-tit">음식을 선택해주세요</h2>
+        <div className="food-group">
+          <ul className="food-category">
+            <li className="group-name">
+              <Link to="#">
+                <span className="name">한식</span>
+              </Link>
+            </li>
+            <li className="group-name">
+              <Link to="#">
+                <span className="name">중식</span>
+              </Link>
+            </li>
+            <li className="group-name">
+              <Link to="#">
+                <span className="name">일식</span>
+              </Link>
+            </li>
+            <li className="group-name">
+              <Link to="#">
+                <span className="name">양식</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainApp;
