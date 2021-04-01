2e6cef9a6e1dccc9ce21a6a34fb412ac3944a7df
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-01 15:35:49 +0900

    UI 수정중

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -4,21 +4,30 @@ import "./Header.css";
 const Header = () => {
   return (
     <header id="header">
-        <div className="inner">
-            <div className="left">
-                <h1><a href="">아무거나</a></h1>
-                <div className="search_container">
-                    <form action="">
-                        <label for="search"></label>
-                        <input type="text" id="search" placeholder="검색" autocomplete="off" />
-                        <button type="submit">
-                            
-                        </button>
-                    </form>
-                </div>
-            </div>
-
+      <div className="inner">
+        <div className="left">
+          <h1>
+            <a href="">아무거나</a>
+          </h1>
         </div>
+
+        <ul className="game-list">
+
+          <li>음식 사다리</li>
+
+          <li>음식 룰렛</li>
+
+          <li>음식 월드컵</li>
+        </ul>
+
+        <div>
+        <ul className="accounts-list">
+          <li>회원가입</li>
+          <li>로그인</li> {/* 로그인 되면 여기에 개발자 소개 나오기 , 그룹생성, 그룹검색은 메인페이지 안에 */}
+        </ul>
+      </div>
+      </div>
+      
     </header>
   );
 };
--- src/layouts/Side/Side.js
+++ src/layouts/Side/Side.js
@@ -1,13 +1,38 @@
-import React from "react"
-import "./Side.css"
+import React from "react";
+import "./Side.css";
 
 const Side = () => {
+  return (
+    <div id="side">
+      <div className="side-head">
+        <div className="tit">이도현</div>
+      </div>
 
-    return(
-        <div id="side">
-            dsa
+      <div className="side-menu side-group">
+        <div className="tit">
+          <span>내 그룹</span>
         </div>
-    )
-}
+        <ul>
+          <li>은행팸</li>
+          <li>인천대18</li>
+          <li>배곧유치원</li>
+        </ul>
+      </div>
 
-export default Side
+      <div className="side-menu recent-ate">
+        <div className="tit">
+          <span>최근에 먹은 것</span>
+        </div>
+        <ul>
+          <li>피자</li>
+          <li>햄버거</li>
+          <li>스테이크</li>
+          <li>토스트</li>
+          <li>물</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Side;
